7837d386adfcf9f3e25d962d145851e556690b19
Manideepnizam26 <manideep.nizam@iiitb.org>
2020-05-29 09:32:19 +0530

    Adding errors to test

--- src/add.py
+++ src/add.py
@@ -9,8 +9,9 @@ class Calculator:
         return a+b
 
 if __name__ == '__main__':
+
     number_of_params_passed = len(sys.argv)
-    a = int(sys.argv[1])
+   a = int(sys.argv[1])
     b = int(sys.argv[2])
 
     print("a:",a)
